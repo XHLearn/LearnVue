2319ac1c0f26e7fae4602cc20327818900781cea
xiaohao <hao.xiao@narwal.com>
2024-05-24 18:03:29 +0800

    learn v-for

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 // import App from './App.vue'
-import App from './xIf.vue'
+// import App from './xIf.vue'
+import App from './xFor.vue'
 
 createApp(App).mount('#app')
