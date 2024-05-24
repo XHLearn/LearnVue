021159611fa2be9a2b2ce12a825532b05b71c8e9
xiaohao <hao.xiao@narwal.com>
2024-05-24 18:31:38 +0800

    for learn4

--- src/xFor.vue
+++ src/xFor.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 // 给每个 todo 对象一个唯一的 id
 let id = 0
@@ -11,8 +11,14 @@ const todos = ref([
   { id: id++, text: 'Learn Vue' }
 ])
 
+const myObject = reactive({
+  title: 'How to do lists in Vue',
+  author: 'Jane Doe',
+  publishedAt: '2016-04-10'
+})
+
 function addTodo() {
-  todos.value.push({id:id++, text:newTodo.value});
+  todos.value.push({ id: id++, text: newTodo.value });
   newTodo.value = ''
 }
 
@@ -23,15 +29,26 @@ function removeTodo(idx) {
 </script>
 
 <template>
+  <ul>
+    <!-- v-for = value, key, index in dict -->
+    <li v-for="(value, key) in myObject">
+      {{ value }} :{{ key }}
+    </li>
+  </ul>
+
   <form @submit.prevent="addTodo">
     <input v-model="newTodo" required placeholder="new todo">
     <button>Add Todo</button>
   </form>
   <br>
   <ul>
+    <!-- v-for = value, index in list -->
     <li v-for="(todo, index) in todos" :key="todo.id">
       {{ todo.id }} : {{ todo.text }}
       <button @click="removeTodo(index)">X</button>
     </li>
   </ul>
+  
+  <!-- idx从1开始 -->
+  <span v-for="n in 10">{{ n }}</span>
 </template>
